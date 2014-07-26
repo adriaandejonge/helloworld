9555d644d574c8521bf96134fe7c63c8fa7e9b81
Yi Wang <yiwang@linkedin.com>
2014-07-25 23:37:19 -0700

    Add flags

--- helloworld.go
+++ helloworld.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 )
 
+var (
+	addr     = flag.String("addr", ":8080", "The network address")
+	greeting = flag.String("greeting", "helloworld", "Greeting message")
+)
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World from Go in minimal Docker container")
+	fmt.Fprintf(w, "Hello from %s", *greeting)
 }
 
 func main() {
+	flag.Parse()
 	http.HandleFunc("/", helloHandler)
 
-	fmt.Println("Started, serving at 8080")
-	err := http.ListenAndServe(":8080", nil)
+	fmt.Printf("Started, serving at %s", *addr)
+	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
 	}
